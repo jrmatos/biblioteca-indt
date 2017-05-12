489f9247854a561e86115f8df7030b6f4429c389
Paulo Matos <paulojomatos@gmail.com>
2017-05-12 03:43:48 -0400

    Integrating hibernate

--- src/main/java/br/com/indt/biblioteca/models/Book.java
+++ src/main/java/br/com/indt/biblioteca/models/Book.java
@@ -2,19 +2,43 @@ package br.com.indt.biblioteca.models;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
  
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Entity
+@Table(name="student")
 public class Book implements Serializable{
      
     private static final long serialVersionUID = 1L;
      
+    @Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
     private Integer id;
+    
+    @Column(name="name")
     private String name;
+    
+    @Column(name="author")
     private String author;
+    
+    @Column(name="publisher")
     private String publisher;
+    
+    @Column(name="year")
     private Integer year;
+    
+    @Column(name="pages")
     private Integer pages;
+    
+    @Column(name="isbn")
     private Integer isbn;
     
     
--- src/main/java/br/com/indt/biblioteca/services/implementations/BookServiceImpl.java
+++ src/main/java/br/com/indt/biblioteca/services/implementations/BookServiceImpl.java
@@ -3,11 +3,14 @@ package br.com.indt.biblioteca.services.implementations;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
- 
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Service;
 
 import br.com.indt.biblioteca.models.Book;
 import br.com.indt.biblioteca.services.BookService;
+import br.com.indt.biblioteca.util.HibernateUtil;
  
 
 @Service
@@ -16,6 +19,12 @@ public class BookServiceImpl implements BookService {
     // create and persist a new book 
     @Override
     public Book create(Book book) {
+    	System.out.println("Book...");
+    	System.out.println(book);
+    	Session session = HibernateUtil.getSessionFactory().openSession();
+    	Transaction t = session.beginTransaction();
+    	session.save(new Book(1, "O melhor do JavaScript", "Douglas Crockford", "O'Reilly", 2008, 170, 564891223));
+    	t.commit();
         return book;
     }
     
